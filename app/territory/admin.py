04246edf59d9408b2870075cc8409a6a329ece4b
akhmad-john <akhmad_shams@mail.ru>
2021-03-25 12:34:10 +0500

    admin added

--- app/territory/admin.py
+++ app/territory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Territory
 # Register your models here.
+
+
+@admin.register(Territory)
+class TerritoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
