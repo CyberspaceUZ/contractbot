7a131a2246f009bd4c9f7f33eaf00c797b4e4a5c
akhmad-john <akhmad_shams@mail.ru>
2021-03-25 12:27:21 +0500

    territory added

---
+++ app/territory/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from app.core.models import TimeStampMixin
+
+# Create your models here.
+
+class Territory(TimeStampMixin):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
