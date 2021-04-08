914bdc854a884c7a329bb80ce817020aaba5f532
maksim <flymr92@gmail.com>
2021-04-08 23:37:39 +0500

    beta v1

--- app/account/tests.py
+++ app/account/tests.py
@@ -5,13 +5,13 @@ from app.account.models import BotUser
 
 class BotUserTestCase(TestCase):
     def setUp(self) -> None:
-        BotUser.objects.create(phone_number='+998944984921', first_name='Abdullah', last_name="Saydumarov",
+        BotUser.objects.create(phone_number='+998944984921', fiull_name='Abdullah',
                                company="Cyberspace", occupation="developer", chat_id="-11100235")
-        BotUser.objects.create(phone_number='+998944984121', first_name='Maxim', last_name="Petrov",
+        BotUser.objects.create(phone_number='+998944984121', full_name='Maxim',
                                company="Cyberspace", occupation="developer", chat_id="-111002444")
 
     def test_botuser_created(self):
-        botuser1 = BotUser.objects.filter(first_name='Abdullah').first()
-        botuser2 = BotUser.objects.filter(first_name='Maxim').first()
+        botuser1 = BotUser.objects.filter(full_name='Abdullah').first()
+        botuser2 = BotUser.objects.filter(full_name='Maxim').first()
         self.assertEqual(botuser1.phone_number, '+998944984921')
         self.assertEqual(botuser2.chat_id, '-111002444')
