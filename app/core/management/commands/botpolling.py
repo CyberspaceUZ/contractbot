cc7b8ca76dfc3b79765bf76b0e8fc8a734b5d5c8
Elbek <allayorovelbek@gmail.com>
2021-04-13 15:35:51 +0500

    botpolling file reformatted to check for pycodestyle

--- app/core/management/commands/botpolling.py
+++ app/core/management/commands/botpolling.py
@@ -6,18 +6,18 @@ from bot.main import init_dispatcher
 
 
 class Command(BaseCommand):
-    help = 'run bot pooling for development only'
+    help = "run bot pooling for development only"
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '--token',
-            action='store',
-            default=os.environ.get('BOT_TOKEN'),
-            help='Telegram bot token (default from env BOT_TOKEN)',
+            "--token",
+            action="store",
+            default=os.environ.get("BOT_TOKEN"),
+            help="Telegram bot token (default from env BOT_TOKEN)",
         )
 
     def handle(self, *args, **options):
-        token = options['token']
+        token = options["token"]
         updater = Updater(token)
         init_dispatcher(updater)
         updater.start_polling()
